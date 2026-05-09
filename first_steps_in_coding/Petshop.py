ddc1ae5f17e2ddfa1bad3d32a68214e92d0473e6
ArtemSerbinov <Serbinoffsky@gmail.com>
2026-05-09 13:07:54 +0300

    Merge branch 'master' of https://github.com/SerbinovArtem/python-basic-april-2026

---
+++ first_steps_in_coding/Petshop.py
@@ -0,0 +1,7 @@
+cat_foog = int(input("amount of packages of cat food: "))
+dog_food = int(input("Amount of packages of dog food : "))
+
+cat_food_price = 4
+dog_food_price = 2.5
+
+print(f"Total price: ",(cat_food_price*cat_foog) + (dog_food_price*dog_food),"$")
