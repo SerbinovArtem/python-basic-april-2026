3360c0ac7ddfcbfa3315d29fe7927f4ed8cdb0a6
ArtemSerbinov <Serbinoffsky@gmail.com>
2026-05-09 11:44:02 +0300

    First commit

---
+++ first_steps_in_coding/Project creation.py
@@ -0,0 +1,6 @@
+name = input()
+project = int(input())
+
+total_hours = project * 3 # required time to finish average project
+
+print(f"It will takes {total_hours} hours to complete all projects.")
